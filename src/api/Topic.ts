f3e1754b261cde1453950d45ecc98c0bc580f1b0
Daniel1464 <Daniel_Chen@caryacademy.org>
2025-05-05 23:53:16 -0400

    Made Tolerance a string instead of an enum

--- src/api/Topic.ts
+++ src/api/Topic.ts
@@ -1,5 +1,7 @@
 import { ComboboxItemGroup } from "@mantine/core"
 
+// We use a enum here so we can have less verbose topics 
+// while having more verbose string representations of them
 export enum Topic {
   CompNumSystems = "Computer Number Systems",
   RecursiveFuncs = "Recursive Functions",
